f8fb0c8e13e7cdfd151afa08208a332513c010f0
dpw1 <diego.boarutto.fortes@gmail.com>
2020-09-08 03:59:51 -0300

    [ADD] routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { HashRouter, Route, Link } from "react-router-dom";
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
 
 function App() {
   return (
@@ -10,14 +22,21 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HashRouter basename="/">
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+            <hr />
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </div>
+        </HashRouter>
       </header>
     </div>
   );
