b0572a9cc885c66b4d2981a24893080628f4012c
dpw1 <diego.boarutto.fortes@gmail.com>
2020-09-08 03:48:00 -0300

    Updates

---
+++ precache-manifest.aed7feef73219a7caa415959070b3ede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e69713e2d49a078aeeaeed160b8f77",
+    "url": "/website-2020/index.html"
+  },
+  {
+    "revision": "8d491f1133987d55aa4c",
+    "url": "/website-2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "25431529bb8dc9b37a9d",
+    "url": "/website-2020/static/js/2.1d312710.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website-2020/static/js/2.1d312710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d491f1133987d55aa4c",
+    "url": "/website-2020/static/js/main.6b22a1fc.chunk.js"
+  },
+  {
+    "revision": "9b5cd21ea1e3c625c1a6",
+    "url": "/website-2020/static/js/runtime-main.651a5696.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website-2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
