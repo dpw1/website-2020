2496701e4126f8b63ff29ad16c02dd774f7e9688
dpw1 <diego.boarutto.fortes@gmail.com>
2020-11-07 22:31:23 -0300

    [ADD] react snap for seo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { hydrate, render } from "react-dom";
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
-);
+const root = document.getElementById("root");
+
+if (root.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    root,
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    root,
+  );
+}
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   root,
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
